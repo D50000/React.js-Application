7c5c4216682289eed7b89e55577e289d82b00556
Eddie Hsu <aoe102198@gmail.com>
2019-09-30 19:42:13 +0800

    update
    update StorePicker.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,15 +3,35 @@ import { getFunName } from "../helpers";
 
 // Declare a 'StorePicker Component' Class. 
 class StorePicker extends React.Component{
+    constructor(){
+        // Need to super() first, if the current class is extends from other class.
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    myInput = React.createRef();
+
+    // "This" in the new declare method can't reference back to the original class components.
+    goToStore(event){
+        // 1. Stop the form from submitting.
+        event.preventDefault();
+        // 2. Get the text from that input.Ｂut don't "select" or "modify" any DOM in react framework.
+        const storeName = this.myInput.current.value;
+
+        // 3. Use the Router to change the page to /store/xxxxxxx. 
+        this.props.history.push(`/store/${storeName}`);
+    }
+
     render(){
         return (
             <Fragment>
                 {/* Here is comment */}
                 <p>Buy It Now !！！！！！！!</p>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter A Store ~</h2>
                     <input
                         type="text"
+                        ref={this.myInput}
                         required
                         placeholder="Store Name"
                         defaultValue={getFunName()}
